3cd7c87f56c5b354d2e72a194049dd191bbdd93a
hagtpsjt <hagtpsjt@gmail.com>
2021-01-23 21:55:14 +0900

    update docs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Index msg="테스트입니다."/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from './components/page/Index';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
---
+++ src/components/page/Index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="wrap">
+        <p>초기화면</p>
+        <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "IndexPage",
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
